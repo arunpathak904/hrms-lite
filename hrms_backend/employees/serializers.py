d4b462901aff16c9a64c34d021731c62fdd82a49
Arun pathak <arunpathak904@gmail.com>
2026-01-30 02:18:31 +0530

    Fix attendance to accept employee_id instead of FK id

--- hrms_backend/employees/serializers.py
+++ hrms_backend/employees/serializers.py
@@ -7,12 +7,29 @@ class EmployeeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+from rest_framework import serializers
+from .models import Employee, Attendance
+
 class AttendanceSerializer(serializers.ModelSerializer):
+    employee_id = serializers.CharField(write_only=True)
     employee_name = serializers.CharField(
-        source='employee.full_name',
+        source="employee.full_name",
         read_only=True
     )
 
     class Meta:
         model = Attendance
-        fields = '__all__'
+        fields = ['id', 'employee_id', 'employee', 'employee_name', 'date', 'status']
+        read_only_fields = ['employee']
+
+    def validate_employee_id(self, value):
+        if not Employee.objects.filter(employee_id=value).exists():
+            raise serializers.ValidationError("Employee not found")
+        return value
+
+    def create(self, validated_data):
+        employee_id = validated_data.pop('employee_id')
+        employee = Employee.objects.get(employee_id=employee_id)
+        validated_data['employee'] = employee
+        return super().create(validated_data)
+
